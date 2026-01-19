dfb696b3083f98d0607274bcf28f7bbfb18f81fd
BarneyBoy232 <ethan.barnacoat@gmail.com>
2026-01-19 22:02:25 +1100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,17 +1,24 @@
 @echo off
-:: This file is for the target PCs. Run as Administrator.
+:: Run this as Administrator on the 50 Target PCs
+:: It installs the screen control (RustDesk) and the remote management (WinRM)
 
-echo [1/4] Enabling Remote Management...
+echo [1/4] Unlocking Remote Management...
 powershell -Command "Enable-PSRemoting -Force"
-
-echo [2/4] Allowing Remote Admin access...
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
-
-echo [3/4] Setting network to Private for firewall bypass...
 powershell -Command "Set-NetConnectionProfile -NetworkCategory Private"
 
-echo [4/4] Installing Chocolatey package manager...
+echo [2/4] Installing Package Manager...
 powershell -NoProfile -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 
-echo PREP COMPLETE.
+echo [3/4] Installing Chrome & RustDesk...
+set "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install googlechrome rustdesk -y
+
+echo [4/4] Setting Remote Access Password...
+:: This sets the password you will use on your Main PC to log in
+if exist "C:\Program Files\RustDesk\rustdesk.exe" (
+    "C:\Program Files\RustDesk\rustdesk.exe" --password "Control123!"
+)
+
+echo SETUP COMPLETE.
 pause
